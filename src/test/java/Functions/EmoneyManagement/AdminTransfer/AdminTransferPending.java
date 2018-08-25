048ddb0e65f105faa68dc7856a2a2f57ac2a95a2
izhdanova46@gmail.com <Cheeseburger18>
2018-08-25 11:42:49 +0300

    Static fields

--- src/test/java/Functions/EmoneyManagement/AdminTransfer/AdminTransferPending.java
+++ src/test/java/Functions/EmoneyManagement/AdminTransfer/AdminTransferPending.java
@@ -5,13 +5,13 @@ import Functions.EmoneyManagement.AdminTransfer.AdminTransferCreation;
 
 public class AdminTransferPending {
 
-    private static final  String txnId = AdminTransferCreation.getTxnId();
+    private static final  String TXN_ID = AdminTransferCreation.getTxnId();
     public static  final By PENDING_ADMIN_TRANSFER = By.xpath("//*[@translate='general_label_pending_admin_transfers']");
     public static final By SEARCH_BY_FILTER_CRITERIA = By.xpath("//*[@translate='view_search_by_criteria']");
     public static final By TRANSACTION_ID = By.xpath("//*[@id='transactionIdInput']");
     public static final By SEARCH = By.xpath("//*[@ng-click='searchTransactions()']");
 
     public static String getTxnId() {
-        return txnId;
+        return TXN_ID;
     }
 }
