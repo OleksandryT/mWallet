66b7f646d6bf6f2622e3327cac89457062a05fb9
ilonazhd <izhdanova46@gmail.com>
2018-09-03 09:25:13 +0300

    Finished Admin Transfer approval

--- src/test/java/Functions/EmoneyManagement/AdminTransfer/AdminTransferPending.java
+++ src/test/java/Functions/EmoneyManagement/AdminTransfer/AdminTransferPending.java
@@ -3,14 +3,13 @@ package Functions.EmoneyManagement.AdminTransfer;
 import RandomValuesForTests.BackOfficeUser;
 import Tests.Smoke.EmoneyManagement.TestAdminTransfer;
 import org.apache.xpath.jaxp.JAXPVariableStack;
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
 import Functions.EmoneyManagement.AdminTransfer.AdminTransferCreation;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.nio.ch.DefaultAsynchronousChannelProvider;
+
 import static RandomValuesForTests.RandomValues.*;
 
 
@@ -33,13 +32,14 @@ public class AdminTransferPending extends BackOfficeUser {
     public static final By CLEAR = By.xpath("//*[@translate=\"general_button_clear\"]");
     public static final By START_DATE_CALENDAR_PICKER = By.xpath("//*[@id=\"datetimepicker1\"]/span/span");
     public static final By END_DATE_CALENDAR_PICKER = By.xpath("//*[@id=\"datetimepicker2\"]/span");
-    public static final By ADMIN_TXN_ID_TABLE = By.xpath("//*[@id=\"manage\"]/table");
+    public static final By ADMIN_TXN_ID_TABLE = By.xpath("//*[@id=\"manage\"]/table/tbody");
     public static final By NOTES = By.xpath("//*[@name='regAdmnNotes2']");
     public static final By CONFIRM_BOTTOM = By.xpath("//div[@class=\"div3 ng-scope\"]/p/button[@id='admntxnfrConfirm']");
     public static final By REJECT_BOTTOM = By.xpath("//div[@class=\"div3 ng-scope\"]/p/button[@id='admntxnfrReject']");
     public static final By CANCEL_BOTTOM = By.xpath("//div[@class=\"div3 ng-scope\"]/p/button[@id='admnDtlCancel']");
-    public static final By DETAILS = By.xpath("//*[@id=\"manage\"]/table/tbody/tr[2]/td[9]/a/span");
+    public static final By DETAILS = By.xpath("//*[@id=\"manage\"]/table/tbody/tr[2]/td[9]/a");
     public static final By TXN_ID = By.xpath("//*[@id=\"content\"]/div[4]/div/div/section[3]/div/form/div/div/div/label");
+    public static By FIRST_ROW_WITH_TRANSACTION = By.xpath("//*[@id=\"manage\"]/table/tbody/tr[2]/td[1]");
 
     @Override
     public void goToMWallet(String url) {
@@ -106,28 +106,18 @@ public class AdminTransferPending extends BackOfficeUser {
         Actions action = new Actions(driver);
         action.moveToElement(searchButton).click().build().perform();
         System.out.println("Start searching for specific txnId");
-//        Find table
-        WebElement trxTable = driver.findElement(ADMIN_TXN_ID_TABLE);
-//        Locate rows of table
-        List<WebElement> rows_table = trxTable.findElements(By.tagName("tr"));
-//         To calculate no of rows in the table
-        int rows_count = rows_table.size();
-//        Loop will execute till the last row
-        for (int row = 0; row < rows_count; row++) {
-//            To locate columns(cells) of that specific row.
-            List<WebElement> columns_row = rows_table.get(row).findElements(By.tagName("td"));
-//            To calculate columns(cells) of that specific row.
-            int columns_count = columns_row.size();
-//            Iterate through the columns within particular row
-            for (int i = 0; i < columns_count; i++) {
-                String cellText = columns_row.get(i).getText();
-                if (cellText == driver.findElement(TRANSACTION_ID).getAttribute("value")){
+        String firstRowFound = driver.findElement(FIRST_ROW_WITH_TRANSACTION).getText();
+        String current_trx = driver.findElement(TRANSACTION_ID).getAttribute("value");
+        System.out.println("Found is : "+firstRowFound+" "+"Current is "+current_trx);
+                if (firstRowFound.equals(current_trx)){
+                    WebDriverWait wait  = new WebDriverWait(driver, 30);
+                    wait.until(ExpectedConditions.elementToBeClickable(DETAILS));
                     driver.findElement(DETAILS).click();
                     System.out.println("TxnID was found between pendings and details view has been opened");
                 }
+                else
+                    System.out.println("The transaction was not found in the pending admin transfers!");
             }
-        }
-    }
 
     public  void setConfirmBottom (){
         driver.findElement(CONFIRM_BOTTOM).click();
@@ -140,8 +130,6 @@ public class AdminTransferPending extends BackOfficeUser {
     public  void setNotes (){
         driver.findElement(NOTES).sendKeys("autoTest"+RANDOM_ALPHABETIC);
     }
-
-
 
 }
 
